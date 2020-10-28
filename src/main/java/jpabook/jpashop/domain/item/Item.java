159bd0a650bee9ca2e0a318371a75bb81d1de9c5
zidol <ajh3166@naver.com>
2020-10-28 22:20:56 +0900

    상품 비지니스 로직, 레파지토리 로직

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoghStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //비지니스 로직
+    /*
+    stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    /*
+    stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoghStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
