347bbe276cd9b26b518e15dd44b9702ad14e966d
zidol <ajh3166@naver.com>
2020-11-25 23:32:47 +0900

    엔티티를 DTO로 변환 - 페이징과 한계 돌파
    hibernate.default_batch_fetch_size , @BatchSize

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -111,4 +111,13 @@ public class OrderRepository {
                         " join fetch oi.item i", Order.class)
                 .getResultList();
     }
+    //to one 관계는 fetch join, default_batch_fetch_size: 100
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery("select o from Order o" +
+                    " join fetch o.member m" +
+                    " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
